3623710e5713373c5a0673fe6f3471d700057e48
nithin6743 <nithinkumarsottagari03@gmail.com>
2025-01-21 20:48:58 +0530

    added scroll effect for sections and navbar

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
 // Going to particular sections by clicking on the nav links 
-let sections = document.querySelector("section");
-let navlinks = document.querySelector("header nav ul li a");
+let sections = document.querySelectorAll("section");
+let navlinks = document.querySelectorAll("header nav ul li a");
 
 window.onscroll = () => {
+    let scrollPosition = window.scrollY;
+
     sections.forEach(sec => {
-        let top = window.screenY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getattribute("id");
+        let id = sec.getAttribute("id");
 
-        if(top >= offset && top < offset + height) {
+        if(scrollPosition >= offset && scrollPosition < offset + height) {
             navlinks.forEach(links =>{
                 links.classList.remove("active");
-                document.querySelector("header nav a[href*=" + id + "]").classList.add("active");
+                if(links.getAttribute("href").includes(id)){
+                    links.classList.add("active");
+                }
             });
-        };
+        }
         
     });
 };
