26e0c6e0b4b97c667d2b83574e1aa85fa8c7de1c
nithin6743 <nithinkumarsottagari03@gmail.com>
2025-02-23 15:13:50 +0530

    major changes i.e updating the background, display style and added home,about section placeholders

--- script.js
+++ script.js
@@ -35,16 +35,60 @@ function ShowSideBar() {
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "flex";
 }
+
 // to hide sidebar
 function hideSideBar() {
   event.preventDefault();
   const sidebar = document.querySelector(".sidebar");
   sidebar.style.display = "none";
 }
+
 // to close the sidebar when clicked at navlinks
 document.querySelectorAll(".sidebar a").forEach((link) => {
   link.addEventListener("click", () => {
     const sidebar = document.querySelector(".sidebar");
     sidebar.style.display = "none";
   });
+});
+
+//to blur the sections when scrolen through navbar
+window.addEventListener("scroll", function () {
+  let sections = document.querySelectorAll("section");
+  let scrollPosition = window.scrollY + window.innerHeight - 200;
+
+  sections.forEach((section) => {
+    let offset = section.offsetTop;
+    let height = section.offsetHeight;
+
+    if (scrollPosition > offset && scrollPosition < offset + height) {
+      section.classList.add("active");
+      section.classList.remove("faded");
+    } else {
+      section.classList.remove("active");
+      section.classList.add("faded");
+    }
+  });
+});
+
+//menubar in the about section
+document.addEventListener("DOMContentLoaded", () => {
+    const buttons = document.querySelectorAll(".tab-button");
+    const contents = document.querySelectorAll(".tab-content");
+
+    buttons.forEach(button => {
+        button.addEventListener("click", () => {
+            buttons.forEach(button => button.classList.remove("active"));
+            contents.forEach(content => content.classList.remove("active"));
+
+            button.classList.add("active");
+            const targetContent = document.querySelector(`.${button.dataset.target}`);
+            if (targetContent) {
+                targetContent.classList.add("active");
+
+            }
+        });
+    });
+    document.querySelector(".skillsContent").classList.add("active");
+    document.querySelector(".tab-button").classList.add("active");
+
 });
